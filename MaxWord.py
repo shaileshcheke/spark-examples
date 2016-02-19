aa17cad70c6d6664cc4072549c5f18cac001db24
shailesh cheke <shailesh.cheke@gmail.com>
2016-02-19 12:06:13 +0545

    unnecessary print statement is deleted

--- MaxWord.py
+++ MaxWord.py
@@ -4,8 +4,6 @@ rddD=sc.textFile("hdfs://localhost:54310/data/hadoop/spark-input/wc.txt")
 rddS=sc.textFile("hdfs://localhost:54310/data/hadoop/spark-input/stopwords_en.txt")
 flM=rddD.flatMap(lambda x: x.split()).map(lambda x: (x.lower(),1)).reduceByKey(lambda x,y:x+y)
 flS=rddS.flatMap(lambda x: x.split("\n")).map(lambda x: (x,1)).reduceByKey(lambda x,y:x+y)
-for i in flS.collect():
-	print i
 flM=flM.subtractByKey(flS)
 def comp(x,y):
 	if x[1]<y[1]:
